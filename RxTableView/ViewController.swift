302dfc605489a32993104d6229da36eade4adfd9
Nurlybek Karshiga <nurlybekkarshiga@gmail.com>
2020-08-13 11:46:10 -0400

    Initial Commit

---
+++ RxTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxTableView
+//
+//  Created by Nulrybek Karshyga on 8/13/20.
+//  Copyright © 2020 Nulrybek Karshyga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
